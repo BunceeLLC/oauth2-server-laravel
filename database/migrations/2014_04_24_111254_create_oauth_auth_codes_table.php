c8c33bd1c8f34f2966a81388cdb24e81f49c89af
Aditya Balwani <adibalwani@gmail.com>
2019-06-28 12:15:19 -0400

    Added missing import

--- database/migrations/2014_04_24_111254_create_oauth_auth_codes_table.php
+++ database/migrations/2014_04_24_111254_create_oauth_auth_codes_table.php
@@ -9,6 +9,7 @@
  * file that was distributed with this source code.
  */
 
+use App\Models\ClientModel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
